85515a05df3a91867c3b191b1600285924b202fc
PierreBoutin <pierre.boutin@viacesi.fr>
2019-09-24 14:33:42 +0200

    Add read and write files function

--- src/main/java/view/projet_grue.java
+++ src/main/java/view/projet_grue.java
@@ -1,10 +1,18 @@
 package view;
 
+import controler.Wkf_decrypt;
+
 public class projet_grue {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hello");
+		Wkf_decrypt wkf_decrypt = new Wkf_decrypt();
+		wkf_decrypt.writeFiles("D:/Application/tex.txt", "J'aime regarder les filles qui marchent sur la plage");
+		
+		String texte;
+		texte = wkf_decrypt.readFiles("D:/Application/tex.txt");
+		System.out.println(texte);
 	}
 
 }
