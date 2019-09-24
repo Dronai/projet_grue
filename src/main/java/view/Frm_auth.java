64312a6045854db6e92b5e9045d44556cf4aa90d
PierreBoutin <pierre.boutin@viacesi.fr>
2019-09-24 12:44:48 +0200

    Change git ignore and add main

--- src/main/java/view/Frm_auth.java
+++ src/main/java/view/Frm_auth.java
@@ -1,5 +1,5 @@
 package view;
 
 public class Frm_auth {
-
+	
 }
---
+++ src/main/java/view/projet_grue.java
@@ -0,0 +1,10 @@
+package view;
+
+public class projet_grue {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+	}
+
+}
