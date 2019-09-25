dc9022f743d546455fbe280b619ef120a16c471c
PierreBoutin <pierre.boutin@viacesi.fr>
2019-09-25 14:26:11 +0200

    Add logical algorithm of checking

--- src/modele/Map_Dic.java
+++ src/modele/Map_Dic.java
@@ -1,5 +1,31 @@
 package modele;
 
+import java.util.ArrayList;
+
 public class Map_Dic {
 
+	ArrayList<String> wordtab = new ArrayList<String>();
+	CAD cad = new CAD();
+	
+	//Make map => call request. With result -> create array.
+	public Map_Dic() {
+		wordtab.add("Pomme");
+		wordtab.add("Poire");
+		wordtab.add("Fruit");
+		wordtab.add("Banane");
+		wordtab.add("Elephant");
+		wordtab.add("Ours");
+		wordtab.add("Singe");
+	}
+	
+	public boolean compare(String word) {
+		
+		for(int i=0; i<wordtab.size(); i++) {
+			if(wordtab.get(i) == word) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
 }
